ac7ea5a39c4a7925200bd1a639504d12f1e47aff
LeoGuimas <gleo.dev0@gmail.com>
2025-05-12 23:36:40 -0300

    Add lucide-react dependency and refactor components
    - Added "lucide-react" as a dependency in package.json and package-lock.json.
    - Removed unused CSS files (App.css, index.css).
    - Created Container and Heading components with respective styles.
    - Updated App component to use new Container and Heading components.
    - Implemented Home component with a button and TimerIcon.
    - Introduced global and theme styles for consistent design.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading } from "./components/Heading";
+import { Home } from "./components/Home";
+import { Container } from "./components/Container";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./styles/theme.css";
+import "./styles/global.css";
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
 
-export default App
+      <Container>
+        <section>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque
+          voluptatibus reprehenderit praesentium ipsam vel natus debitis! Ullam
+          voluptatem tempore ex culpa, iste aut tenetur, atque quaerat fuga
+          vitae, aliquam repellat.
+        </section>
+        <Heading>
+          <Home />
+        </Heading>
+      </Container>
+    </>
+  );
+}
